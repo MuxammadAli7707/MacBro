3a09a4413b6327ca6f5e189f98ca85291138c716
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-04-12 00:35:07 +0500

    initial commit

---
+++ js/mac.js
@@ -0,0 +1,78 @@
+const elRamSixth = document.getElementById("ram-sixth");
+const elTitle = document.getElementById("title");
+const elMemoryFirst = document.getElementById("memory-first");
+const elMemorySecond = document.getElementById("memory-second");
+const elOneTb = document.getElementById("memory-third");
+const elRamEight = document.getElementById("ram-eight");
+const elEight = document.getElementById("eight");
+const elMemory = document.getElementById("memory");
+const elColor = document.getElementById("colr");
+const elGold = document.getElementById("gold");
+const elSilver = document.getElementById("silver");
+const elGray = document.getElementById("gray");
+
+elRamSixth.addEventListener("click", ()=> {
+  elEight.textContent = "16";
+  elRamSixth.classList.add("active");
+  elRamEight.classList.remove("active");
+  elOneTb.style.display = "block";
+  elRamEight.style.display = "block";
+});
+
+elRamEight.addEventListener("click", ()=>{
+  elEight.textContent = "8";
+  elMemory.textContent = "256 ";
+  elRamSixth.classList.remove("active");
+  elRamEight.classList.add("active");
+  elOneTb.style.display = "none";
+})  
+
+elMemoryFirst.addEventListener("click", ()=> {
+  elMemory.textContent = "256 ";
+  elMemoryFirst.classList.add("active");
+  elMemorySecond.classList.remove("active");
+  elOneTb.classList.remove("active");
+  elOneTb.style.display = "block";
+})
+
+elMemorySecond.addEventListener("click", ()=> {
+  elMemory.textContent = "512 ";
+  elMemoryFirst.classList.remove("active");
+  elMemorySecond.classList.add("active");
+  elOneTb.classList.remove("active");
+  elOneTb.style.display = "block";
+  elRamEight.style.display = "block";
+})
+
+elOneTb.addEventListener("click", ()=> {
+  elMemory.textContent = "1tb";
+  elMemoryFirst.classList.remove("active");
+  elMemorySecond.classList.remove("active");
+  elOneTb.classList.add("active");
+  elRamEight.style.display = "none";
+  elRamSixth.classList.add("active");
+  elOneTb.style.display = "block";
+})
+
+elSilver.addEventListener("click", ()=> {
+  elColor.textContent = "Silver";
+  elSilver.classList.add("active");
+  elGold.classList.remove("active");
+  elGray.classList.remove("active");
+  elOneTb.style.display = "block"; 
+});
+
+elGray.addEventListener("click", ()=> {
+  elColor.textContent = "Space Gray";
+  elSilver.classList.remove("active");
+  elGold.classList.remove("active");
+  elGray.classList.add("active");
+  elOneTb.style.display = "block"; 
+})
+
+elGold.addEventListener("click", ()=> {
+  elColor.textContent = "Gold";
+  elSilver.classList.remove("active");
+  elGold.classList.add("active");
+  elGray.classList.remove("active");
+})
---
+++ js/main.js
@@ -0,0 +1,92 @@
+const macObject = [
+  {
+    id:1,
+    title: 'MacBook Air 13-inch',
+    color: 'gold',
+    ram: {
+     GB8:[{
+       memory:'256GB',
+       price: 12497000
+     },
+     {
+      memory:'512GB',
+      price: 15066000
+    }],
+     GB16: [{
+      memory:'256GB',
+      price: 16935000
+    },
+    {
+     memory:'512GB',
+     price: 19270500
+   },
+   {
+    memory:'1TB',
+    price: 20438500
+  }]
+    },
+    imgs: [
+      '../imgs/mac-gold-face.webp',
+      '../imgs/mac-gold-key.webp',
+      '../imgs/mac-gold-command.webp',
+      '../imgs/mac-gold-left.webp',
+      '../imgs/mac-gold-book.webp',
+    ],
+  },
+  {
+    id:2,
+    title: 'MacBook Air 13-inch',
+    color: 'silver',
+    ram: {
+     GB8:[{
+       memory:'256GB',
+       price: 12497000
+     },
+     {
+      memory:'512GB',
+      price: 15066000
+    }],
+     GB16: [{
+      memory:'256GB',
+      price: 16935000
+    },
+    {
+     memory:'512GB',
+     price: 19270500
+   },
+   {
+    memory:'1TB',
+    price: 21606500
+  }]
+    },
+    imgs: ['images/mac-silver.webp','images/mac-silver2.webp','images/mac-silver3.webp','images/mac-silver4.webp','images/mac-silver5.webp']
+  },
+  {
+    id:3,
+    title: 'MacBook Air 13-inch',
+    color: 'space grey',
+    ram: {
+     GB8:[{
+       memory:'256GB',
+       price: 12497000
+     },
+     {
+      memory:'512GB',
+      price: 15066000
+    }],
+     GB16: [{
+      memory:'256GB',
+      price: 16935000
+    },
+    {
+     memory:'512GB',
+     price: 19270500
+   },
+   {
+    memory:'1TB',
+    price: 21606500
+  }]
+    },
+    imgs: ['images/mac-grey.webp','images/mac-grey2.webp','images/mac-grey3.webp','images/mac-grey4.webp','images/mac-grey5.webp']
+  }
+];
